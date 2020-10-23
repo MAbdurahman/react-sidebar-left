7a2b46a0d43ca424187236af2c1d5e4ace288d90
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-23 16:45:12 -0500

    Modify Navbar.css, Sidebar.css, SidebarBackground.css, and SidebarBackground.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar';
+import SidebarBackground from './components/SidebarBackground/SidebarBackground'
 
 class App extends Component {
 
+  state = {
+    isSidebarOpen: false
+
+  };
+
+  sidebarToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {isSidebarOpen: !prevState.isSidebarOpen};
+    });
+  };
+
 
   render() {
+    let sidebar;
+    let background;
+
     return (
       <div className="wrapper">
         <Navbar/>
         <Sidebar/>
+        <SidebarBackground/>
         <main>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, distinctio.</p>
         </main>
---
+++ src/components/SidebarBackground/SidebarBackground.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SidebarBackground.css';
+
+const SidebarBackground = props => {
+   return (
+      <div className="sidebar__background">
+         
+      </div>
+   );
+};
+
+export default SidebarBackground;
